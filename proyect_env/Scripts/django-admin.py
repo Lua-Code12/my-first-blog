3126aad5cb1285f9c66cfa57859f209092de383e
lua <camiandrea91@hotmail.com>
2020-01-21 22:56:05 -0300

    apertura en otro pc presenta error

---
+++ proyect_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sonne\proyecto_web\my-first-blog\proyect_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
